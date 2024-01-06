4fbe72c5d1f49bbc25a51ebb33ceb8476d6a2369
Haeze-Trash <146194334+Haeze-Trash@users.noreply.github.com>
2024-01-07 00:28:09 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,39 @@
+local players = game:GetService("Players")
+local plr = players.LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local attackremote = ReplicatedStorage.ReplicatedPackage.Remotes.action
+local mobs = {
+	"Bandit",
+	"Lowly Goblin",
+	"Mage Apprentice",
+	"Ogre",
+	"Shadow Hand Goon",
+	"Seasoned Mercenary",
+	"Alpha Wolf",
+	"Vennum",
+	"Wind Monk",
+	"Wolf",
+	"Wolf Hunter",
+	"Yetti",
+}
+local function mobfarm()
+	if not Settings.Autofarm.Toggle then
+		return
+	end
+	for i, v in next, workspace.Entities.Enemies:GetChildren() do
+		if checker(v, Settings.Autofarm.Mob, "mob") then
+			plr.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
+				* CFrame.new(0, Settings.Autofarm.Position, 0)
+				* CFrame.Angles(math.rad(angle(Settings.Autofarm.Position)), 0, 0)
+			attackremote:FireServer("M1")
+		end
+	end
+end
 function d(dn) ldit = dn % 10 if ldit == 1 and dn ~= 11 then return 'st' elseif ldit == 2 and dn ~= 12 then return 'nd' elseif ldit == 3 and dn ~= 13 then return 'rd' else return 'th' end; end;
 function timef(dtt,dt) dtt = string.gsub(dtt,"%%o",d(dt.day)) return os.date(dtt,os.time(dtdt)) end; u0name = os.date("*t",os.time())
 local Fluent = loadstring(game:HttpGet("https://pastebin.com/raw/pngpMWrf"))()
-local SaveManager = loadstring(game:HttpGet("https://pastebin.com/raw/gSKcEu81"))()
-local InterfaceManager = loadstring(game:HttpGet("https://pastebin.com/raw/pS2Nvew8"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 --  - %A, %B %d%o, %Y.
 local Window = Fluent:CreateWindow({
     Title = timef("Haeze Hub Normal Scripts", u0name),
@@ -14,54 +45,28 @@ local Window = Fluent:CreateWindow({
     MinimizeKey = Enum.KeyCode.LeftControl
 })
 local Tabs = {
-    Main = Window:AddTab({ Title = "General", Icon = "" }),
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
 local Options = Fluent.Options do
-    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Auto Teleport to Upstairs", Default = false });Toggle:OnChanged(function(Value)
-    _G.Teleport_to_HighestPoint = Value
-        while _G.Teleport_to_HighestPoint do wait(0.1)
-            for i,v in pairs(game:GetService("Workspace").tower.finishes:GetChildren()) do
-                if i == 1 and v.Name == "Finish" then
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position)
-                end;
-            end;
-        end;
+    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Mob Farm toggle", Default = false });Toggle:OnChanged(function()
+        Settings.Autofarm.Toggle = Value
     end);Options.MyToggle:SetValue(false)
-    Tabs.Main:AddButton({Title = "Teleport to Upstairs", Callback = function()
-        for _,v in pairs(game:GetService("Workspace").tower.finishes:GetChildren()) do
-            if _ == 1 and v.Name == "Finish" then
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position)
-            end;
-        end;
-    end});
-    Tabs.Main:AddButton({Title = "Disable Anticheat", Callback = function()
-        for _,v in pairs(getgc()) do
-            if type(v) == "function" and getfenv(v).script == game:GetService("Players").LocalPlayer.PlayerScripts.LocalScript then
-                if debug.getinfo(v).name == "kick" then
-                    hookfunction(v, function(...)
-                    end);
-                end;
-            end;
-        end;
-        for _,v in pairs(getgc()) do
-            if type(v) == "function" and getfenv(v).script == game:GetService("Players").LocalPlayer.PlayerScripts.LocalScript2 then
-                if debug.getinfo(v).name == "FindFirstChild" then
-                    hookfunction(v, function(...)
-                    end);
-                end;
-            end;
-        end;game:GetService("Players").LocalPlayer.PlayerScripts.LocalPartScript:Destroy()
-    end});
-    local Input = Tabs.Main:AddInput("Input", {Title = "Walk Speed", Default = "", Numeric = false, Callback = function(Text)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Text
-    end})
-    local Input = Tabs.Main:AddInput("Input", {Title = "Power Jump", Default = "", Numeric = false, Callback = function(Text)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Text
-    end})
-end;
+    local Dropdown = Tabs.Main:AddDropdown("Dropdown", {Title = "Mobs",Values = {mobs},Multi = false, Default = 1});Dropdown:OnChanged(function(Value)
+        Settings.Autofarm.Mob = Value
+    end);
+    local Slider = Tabs.Main:AddSlider("Slider", {Title = "Position",Default = 10,Min = -15,Max = 15,Rounding = 0,Callback = function(Value)
+        Settings.Autofarm.Position = Value
+    end});Slider:SetValue(10)
+end
 
-SaveManager:SetLibrary(Fluent) InterfaceManager:SetLibrary(Fluent) SaveManager:IgnoreThemeSettings()
-SaveManager:SetIgnoreIndexes({}) InterfaceManager:SetFolder("FluentScriptHub")
-SaveManager:SetFolder("FluentScriptHub/specific-game") InterfaceManager:BuildInterfaceSection(Tabs.Settings)
-SaveManager:BuildConfigSection(Tabs.Settings) Window:SelectTab(1) SaveManager:LoadAutoloadConfig()
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({})
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+Window:SelectTab(1)
+SaveManager:LoadAutoloadConfig()
