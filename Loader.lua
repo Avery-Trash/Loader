44f3b5dc345b7d63068ca768e6dd904bdb1b1272
Haeze-Trash <146194334+Haeze-Trash@users.noreply.github.com>
2024-01-07 12:02:14 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,168 +1,317 @@
-function d(dn)
-    ldit = dn % 10
-    if ldit == 1 and dn ~= 11
-        then return 'st'
-    elseif ldit == 2 and dn ~= 12
-        then return 'nd'
-    elseif ldit == 3 and dn ~= 13
-        then return 'rd'
-    else
-        return 'th'
-    end
-end
-function timef(dtt,dt)
-    dtt = string.gsub(dtt,"%%o",d(dt.day))
-    return os.date(dtt,os.time(dtdt))
-end
-u0name = os.date("*t",os.time())
-
-return function()
-	local emojies = {"😂","😎","😍","😘","🤩","😫","🤑","🤯","🥶","🥵","😇"}
-	local repo = "https://raw.githubusercontent.com/HTDBarsi/LinoriaLib/main/";
-	local Library = (loadstring(game:HttpGet(repo .. "Library.lua")))();
-	local color = BrickColor.new("Really red");
-	local plr = game.Players.LocalPlayer;
-	local empty = Vector3.new();
-	local hit = game.ReplicatedStorage.Remotes.ParryAttempt;
-	local cam = workspace.CurrentCamera;
-	local Settings = {
-		Autoparry = {
-			Toggle = false,
-			Cooldown = 0.1,
-			Range = 0.05,
-			ConstantRange = 5
-		}
-	};
-	local Window = Library:CreateWindow({
-		Title = timef("Scape Hub Premium Editor - %A, %B %d%o, %Y.", u0name),
-		Center = true,
-		AutoShow = true,
-		TabPadding = 8,
-		MenuFadeTime = 0
-	});
-	local Tabs = {
-		Main = Window:AddTab("All features"),
-		["UI Settings"] = Window:AddTab("UI Settings")
-	};
-	local LeftGroupBox = Tabs.Main:AddLeftGroupbox("Main");
-	function getspeed()
-        for i,v in pairs(workspace.Balls:GetChildren()) do 
-            if v.zoomies.VectorVelocity ~= empty then
-                return v.zoomies.VectorVelocity.Magnitude;
-            end
-        end
-    end
-    function estimateTime(ball)
-		return (getspeed() and getdistance(ball) / getspeed()) or 69 
-	end;
-
-	function getdistance(ball)
-		return (ball.Position - plr.Character.HumanoidRootPart.Position).Magnitude
+local players = game:GetService("Players")
+local plr = players.LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local mag
+local attackremote = ReplicatedStorage.ReplicatedPackage.Remotes.action
+local mobs = {
+	"Bandit",
+	"Lowly Goblin",
+	"Mage Apprentice",
+	"Ogre",
+	"Shadow Hand Goon",
+	"Seasoned Mercenary",
+	"Alpha Wolf",
+	"Vennum",
+	"Wind Monk",
+	"Wolf",
+	"Wolf Hunter",
+	"Yetti",
+}
+local bossTable = {
+	"Grasslands Warlord",
+	"Aetherstone Golem",
+	"Bandit Leader",
+	"Riddlebones",
+	"Wolf Hunter Craftsman",
+}
+local questTable = {}
+local flowersTable = {
+	"Fire Lily",
+	"Grass",
+	"Blue Lily",
+	"Mana Flower",
+	"Frozen Lily",
+}
+local Settings = {
+	Autofarm = {
+		Toggle = false,
+		Position = 10,
+		Mob = "",
+	},
+	Autocollect = {
+		Toggle = false,
+		Flowers = false,
+		Item = "Grass",
+	},
+	Players = {
+		Toggle = false,
+		Position = 10,
+	},
+	Notifier = true,
+	Autoquest = {
+		Toggle = false,
+		Position = 10,
+		Quest = "",
+	},
+	Bossfarm = {
+		Toggle = false,
+		Position = 10,
+		Boss = "",
+	},
+}
+local function angle(number)
+	if number >= 0 then
+		return -90
+	else
+		return 90
 	end
-
-	LeftGroupBox:AddToggle("Auto Parry Toggle", {
-		Text = "Auto Parry "..emojies[math.random(1,#emojies)],
-		Default = false,
-		Tooltip = "Automatically hits the ball for you!",
-		Callback = function(Value)
-			Settings.Autoparry.Toggle = Value;
+end
+local FillerNpcs = {}
+local Quests = {}
+local interactions = {}
+local events = {}
+task.spawn(function()
+	if game.PlaceId == 14839995292 then
+		for i, v in next, workspace.Entities.Interactions.FillerNpcs:GetChildren() do
+			table.insert(FillerNpcs, v.Name)
 		end
-	});
 
-	LeftGroupBox:AddSlider("Parry Cooldown", {
-        Text = "Parry Cooldown",
-        Default = 0.1,
-        Min = 0,
-        Max = 1,
-        Rounding = 2,
-        Compact = false,
-        Callback = function(Value)
-            Settings.Autoparry.Cooldown = Value;
-        end
-    })
+		for i, v in next, workspace.Entities.Interactions.Quests:GetChildren() do
+			table.insert(Quests, v.Name)
+		end
 
-	LeftGroupBox:AddSlider("Parry constant range", {
-        Text = "Parry Constant Range",
-        Default = 5,
-        Min = 0,
-        Max = 30,
-        Rounding = 0,
-        Compact = false,
-        Callback = function(Value)
-            Settings.Autoparry.ConstantRange = Value;
-        end
-    })
+		for i, v in next, workspace.Entities.Interactions["Special Interactions"]:GetChildren() do
+			table.insert(interactions, v.Name)
+		end
 
-	LeftGroupBox:AddSlider("Parry range", {
-        Text = "Parry timed range",
-        Default = 0.05,
-        Min = 0,
-        Max = 1,
-        Rounding = 2,
-        Compact = false,
-        Callback = function(Value)
-            Settings.Autoparry.Range = Value;
-        end
-    })
-
-	LeftGroupBox:AddButton({
-		Text = "Blatant aura",
-		Func = function() 
-			local function getspeed()
-				for i,v in pairs(workspace.Balls:GetChildren()) do 
-					if v.zoomies.VectorVelocity ~= empty then
-						return v.zoomies.VectorVelocity;
-					end
-				end
-			end
-			workspace.Balls.ChildAdded:Connect(function(v)
-				v.Changed:Connect(function(prop)
-					c = plr.Character
-					if getspeed().X > getspeed().Z then 
-						c.HumanoidRootPart.CFrame = v.CFrame + Vector3.new(0,5,10);
-					else 
-						c.HumanoidRootPart.CFrame = v.CFrame + Vector3.new(10,5,0);
-					end
-					cam.CameraSubject = v
-					if prop == "BrickColor" and v[prop] == color then
-						while v[prop] == color and v and c.Humanoid.Health ~= 0 do 
-							cnt = 0
-							hit:FireServer(0.5,cam.CFrame,{},workspace.Balls:GetChildren())
-							while v.BrickColor == color and cnt ~= 20 do cnt += 1 task.wait() end
-						end
-						task.wait()
-					end;
-				end);
-			end);
-		end,
-		DoubleClick = false,
-		Tooltip = "Rejoin to stop!"
-	})
-
-	-- Shoutout to bluwu for helping fr
-	local hitcd = false
-	
-	function counter()
-		task.wait(Settings.Autoparry.Cooldown)
-		hitcd = false
+		for i, v in next, workspace.Entities.Interactions.SpecialEvents:GetChildren() do
+			table.insert(events, v.Name)
+		end
+	else
+		return
 	end
-    workspace.Balls.ChildAdded:Connect(function(v)
-        v:GetPropertyChangedSignal("BrickColor"):Connect(function()
-			if v.BrickColor ~= color then 
-				hitcd = false
+end)
+local function checker(object, location, type)
+	if type == "mob" then
+		if location[object.Name] == true and object.Humanoid.Health > 0 then
+			return true
+		else
+			return false
+		end
+	elseif type =="flower" then
+		if location[object:FindFirstChildOfClass("Model").Name] == true then
+			return true
+		else
+			return false
+		end
+	elseif type == "boss" then
+		if location[object.Name] == true then
+			return true
+		else
+			return false
+		end
+	end
+end
+local function mobfarm()
+	if not Settings.Autofarm.Toggle then
+		return
+	end
+	for i, v in next, workspace.Entities.Enemies:GetChildren() do
+		if checker(v, Settings.Autofarm.Mob, "mob") then
+			plr.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
+				* CFrame.new(0, Settings.Autofarm.Position, 0)
+				* CFrame.Angles(math.rad(angle(Settings.Autofarm.Position)), 0, 0)
+			attackremote:FireServer("M1")
+		end
+	end
+end
+local function godmode()
+	ReplicatedStorage.ReplicatedPackage.Remotes.Distance:FireServer(0 / 0, 0 / 0, "High")
+end
+local function autocollect()
+	if not Settings.Autocollect.Toggle then
+		return
+	end
+	for i, v in next, workspace.Entities.Interactions.Collectibles.OreSpawn:GetDescendants() do
+		if v.Name == "OreSpawn" and v:FindFirstChildOfClass("ProximityPrompt") then
+			plr.Character:PivotTo(v:GetPivot())
+			mag = (v.Position - plr.Character.HumanoidRootPart.Position).magnitude
+			if mag < 15 then
+				plr.Character:PivotTo(v:GetPivot())
+				v.ProximityPrompt.HoldDuration = 0
+				fireproximityprompt(v.ProximityPrompt)
 			end
-		end)
-		v:GetPropertyChangedSignal("Position"):Connect(function()
-            if Settings.Autoparry.Toggle and v["BrickColor"] == color then
-				if (estimateTime(v) < Settings.Autoparry.Range or getdistance(v) < Settings.Autoparry.ConstantRange) and not hitcd then 
-					hitcd = true
-					task.spawn(counter)
-					print("Hit",Settings.Autoparry.Range,Settings.Autoparry.ConstantRange,Settings.Autoparry.Cooldown)
-					hit:FireServer(0.5,CFrame.new(),{},{})
-				end
+			break
+		end
+	end
+end
+local function flowers()
+	if not Settings.Autocollect.Flowers then
+		return
+	end
+	for i, v in next, workspace.Entities.Interactions.Collectibles.Flowers:GetDescendants() do
+		if v:FindFirstChildOfClass("ProximityPrompt") and checker(v, Settings.Autocollect.Item, "flower") then
+			plr.Character:PivotTo(v:GetPivot())
+			mag = (v.Position - plr.Character.HumanoidRootPart.Position).magnitude
+			if mag < 15 then
+				plr.Character:PivotTo(v:GetPivot())
+				v.ProximityPrompt.HoldDuration = 0
+				fireproximityprompt(v.ProximityPrompt)
 			end
-			task.wait()
-		end)
-    end)
-	
-end;
+			break
+		end
+	end
+end
+local function farmplayers()
+	if not Settings.Players.Toggle then
+		return
+	end
+	for i, v in next, players:GetChildren() do
+		if v ~= plr and v.Character and v.Character.Humanoid.Health > 0 then
+			plr.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame
+				* CFrame.new(0, Settings.Players.Position, 0)
+				* CFrame.Angles(math.rad(angle(Settings.Players.Position)), 0, 0)
+			attackremote:FireServer("M1")
+		end
+	end
+end
+local function bossnotifier()
+	if not Settings.Notifier then
+		return
+	end
+	for i, v in pairs(workspace.Entities.Enemies:GetChildren()) do
+		if bossTable[v.Name] then
+			game.StarterGui:SetCore("SendNotification", {
+				Title = string.format("Boss Spawned: %s", v.Name), -- the title (ofc)
+				Text = "", -- what the text says (ofc)
+				Icon = "", -- the image if u want.
+				Duration = 25, -- how long the notification should in secounds
+			})
+		end
+	end
+end
+local function autoquest()
+	if not Settings.Autoquest.Toggle then
+		return
+	end
+end
+local function chestfinder(boss)
+	for i,v in pairs(workspace.Entities.Events[boss.Name].Chests:GetDescendants()) do
+		if v:IsA("Model") and v.Name == "Chest"  and v:FindFirstChildOfClass("ProximityPrompt") then
+			plr.Character:PivotTo(v:GetPivot())
+			v.ProximityPrompt.HoldDuration = 0
+			fireproximityprompt(v.ProximityPrompt)
+		end
+	end
+end
+local function bossfarm()
+	if not Settings.Bossfarm.Toggle then
+		return
+	end
+	for i, v in pairs(workspace.Entities.Enemies:GetChildren()) do
+		if checker(v, Settings.Bossfarm.Boss, "boss") then
+			if v.Humanoid.Health > 0 then
+				plr.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
+					* CFrame.new(0, Settings.Autofarm.Position, 0)
+					* CFrame.Angles(math.rad(angle(Settings.Autofarm.Position)), 0, 0)
+				attackremote:FireServer("M1")
+			else
+				chestfinder(v)
+			end
+		end
+	end
+end
+local function autospin()
+	if not  Settings.Autoquest.Toggle then return end;
+end
+function d(dn) ldit = dn % 10 if ldit == 1 and dn ~= 11 then return 'st' elseif ldit == 2 and dn ~= 12 then return 'nd' elseif ldit == 3 and dn ~= 13 then return 'rd' else return 'th' end; end;
+function timef(dtt,dt) dtt = string.gsub(dtt,"%%o",d(dt.day)) return os.date(dtt,os.time(dtdt)) end; u0name = os.date("*t",os.time())
+local Library = (loadstring(game:HttpGet("https://raw.githubusercontent.com/HTDBarsi/LinoriaLib/main/Library.lua")))()
+local ThemeManager = (loadstring(game:HttpGet("https://pastebin.com/raw/R8MDuC3q")))()
+local SaveManager = (loadstring(game:HttpGet("https://raw.githubusercontent.com/HTDBarsi/LinoriaLib/main/addons/SaveManager.lua")))()
+local Window = Library:CreateWindow({Title = timef("Haeze Hub Normal Scripts - %A, %B %d%o, %Y.", u0name),Center = true,AutoShow = true,TabPadding = 8,MenuFadeTime = 0})
+local Tabs = {Main = Window:AddTab("Main"),["UI Settings"] = Window:AddTab("UI Settings"),}
+local TabBox = Tabs.Main:AddLeftTabbox();
+local General = TabBox:AddTab('\\\\ General //');
+General:AddToggle("MyToggle", {Text = "Auto Farm Level",Default = false,Callback = function(Value)
+	Settings.Autofarm.Toggle = Value
+end})
+General:AddDropdown("MyDropdown", {Values = mobs,Default = 1,Multi = true,Text = "Mob's Selected",Callback = function(Value)
+	Settings.Autofarm.Mob = Value
+end})
+General:AddSlider("MySlider", {Text = "- Position Farm I",Default = 10,Min = -15,Max = 15,Rounding = 0,Compact = false,Callback = function(Value)
+	Settings.Autofarm.Position = Value
+end})
+General:AddToggle("MyToggle", {Text = "Auto Farm Bosse's",Default = false,Callback = function(Value)
+	Settings.Bossfarm.Toggle = Value
+end})
+General:AddDropdown("MyDropdown", {Values = bossTable,Default = 1,Multi = true,Text = "Bosse's Selected",Callback = function(Value)
+	Settings.Bossfarm.Boss = Value
+end})
+General:AddSlider("MySlider", {Text = "- Position Farm II",Default = 10,Min = -15,Max = 15,Rounding = 0,Compact = false,Callback = function(Value)
+	Settings.Bossfarm.Position = Value
+end})
+General:AddToggle("MyToggle", {Text = "Auto Farm Ore's",Default = false,Callback = function(Value)
+	Settings.Autocollect.Toggle = Value
+end})
+General:AddDivider()
+General:AddToggle("MyToggle", {Text = "Auto Farm Flower",Default = false,Callback = function(Value)
+	Settings.Autocollect.Flowers = Value
+end})
+General:AddDropdown("MyDropdown", {Values = flowersTable,Default = 1,Multi = true,Text = "Flower's Selected",Callback = function(Value)
+	Settings.Autocollect.Item = Value
+end})
+General:AddToggle("MyToggle", {Text = "Auto Farm Player",Default = false,Callback = function(Value)
+	Settings.Players.Toggle = Value
+end})
+General:AddSlider("MySlider", {Text = "- Position Farm III",Default = 10,Min = -15,Max = 15,Rounding = 0,Compact = false,Callback = function(Value)
+	Settings.Players.Position = Value
+end})
+
+local TabBox = Tabs.Main:AddRightTabbox();
+local Misc = TabBox:AddTab('\\\\ Miscellaneous //');
+Misc:AddDropdown("MyDropdown", {Values = FillerNpcs,Default = 1,Multi = false,Text = "Filler Npc's Teleport",Callback = function(Value)
+	plr.Character:PivotTo(workspace.Entities.Interactions.FillerNpcs[Value]:GetPivot())
+end})
+Misc:AddDropdown("MyDropdown", {Values = Quests,Default = 1,Multi = false,Text = "Quest's Teleport",Callback = function(Value)
+	plr.Character:PivotTo(workspace.Entities.Interactions.Quests[Value]:GetPivot())
+end})
+Misc:AddDropdown("MyDropdown", {Values = interactions,Default = 1,Multi = false,Text = "Special interraction's Teleport",Callback = function(Value)
+	plr.Character:PivotTo(workspace.Entities.Interactions["Special Interactions"][Value]:GetPivot())
+end})
+Misc:AddDropdown("MyDropdown", {Values = events,Default = 1,Multi = false,Text = "Special Event's Teleport",Callback = function(Value)
+	lr.Character:PivotTo(workspace.Entities.Interactions.SpecialEvents[Value]:GetPivot())
+end})
+local MyButton = Misc:AddButton({Text = "Godmode",Func = function()
+	godmode()
+end})
+Misc:AddToggle("MyToggle", {Text = "Bosse's Spawned Notifier",Default = false,Callback = function(Value)
+	Settings.Notifier = Value
+end})
+
+local TeleService = game:GetService("TeleportService");local Players = game:GetService("Players");
+local MenuGroup = Tabs["UI Settings"]:AddLeftGroupbox("Menu");
+(MenuGroup:AddLabel("Menu bind")):AddKeyPicker("MenuKeybind", {Default = "End",NoUI = true,Text = "Menu keybind",})
+MenuGroup:AddButton("Unload", function() Library:Unload() end)
+Library.ToggleKeybind = Options.MenuKeybind
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+SaveManager:IgnoreThemeSettings()
+SaveManager:BuildConfigSection(Tabs["UI Settings"])
+ThemeManager:SetFolder("Haeze(Hub) - Clover Retribution [Scripts]")
+SaveManager:SetFolder("Haeze(Hub) - Clover Retribution [Scripts]")
+ThemeManager:ApplyToTab(Tabs["UI Settings"])
+SaveManager:LoadAutoloadConfig()
+local MenuGroup = Tabs['Configuration']:AddRightGroupbox('          \\\\ Servers Job //');
+MenuGroup:AddInput('MyTextbox', {Default = '',Numeric = false,Finished = false,Text = 'Join ID:',Placeholder = '',Callback = function(Value) _G.Join_ID = Value;end});
+MenuGroup:AddButton("Invite",function() TeleService:TeleportToPlaceInstance(game.PlaceId,_G.Join_ID,Players.LocalPlayer);end);
+MenuGroup:AddButton("Copy Join ID",function() setclipboard(game.JobId);end);getgenv().Library = Library;
+game:GetService("RunService").Heartbeat:connect(function()
+	task.spawn(function()
+		autocollect()
+		flowers()
+		mobfarm()
+		farmplayers()
+		bossnotifier()
+	end)
+end)
